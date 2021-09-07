f5df57b0adfded3d3e312dcc6d5bcb8e0aed0acb
Preocts <preocts@gmail.com>
2021-09-07 06:47:01 -0400

    Self contain test, remove fixture

--- tests/download_itemlist_test.py
+++ tests/download_itemlist_test.py
@@ -1,3 +1,4 @@
+import gzip
 import logging
 import os
 import pathlib
@@ -11,17 +12,34 @@ from eqcharinfo import download_itemlist
 
 TEST_URL = "https://github.com/Preocts/eqcharinfo/blob/main/README.md"
 MOCK_GZ = pathlib.Path("./tests/fixtures/mock_itemlist.txt.gz")
+MOCK_GZ_BODY = "This is a test file"
 
 
 @pytest.fixture(scope="function", name="mockfile")
 def fixture_mockfile() -> Generator[pathlib.Path, None, None]:
-    """Create a temp file that cleans iteself up"""
+    """Create a temp file that cleans itself up, yields Path to file"""
     try:
         file_desc, path = tempfile.mkstemp()
         os.close(file_desc)
         yield pathlib.Path(path)
     finally:
         os.remove(path)
+
+
+@pytest.fixture(scope="function", name="mockgz")
+def fixture_mockgz(mockfile: pathlib.Path) -> Generator[pathlib.Path, None, None]:
+    """Creates a temp `*.txt.gz` file, yields Path to file"""
+    gzfile = mockfile.name + ".txt.gz"
+    gzpath = mockfile.parent / gzfile
+    try:
+        with gzip.open(gzpath, "wb") as gfileout:
+            gfileout.write(MOCK_GZ_BODY.encode())
+
+        yield gzpath
+
+    finally:
+
+        os.remove(gzpath)
 
 
 def test_download_file(mockfile: pathlib.Path, caplog: Any) -> None:
@@ -39,18 +57,17 @@ def test_download_file(mockfile: pathlib.Path, caplog: Any) -> None:
     assert "Skipping download" in caplog.text
 
 
-def test_unpack_file() -> None:
+def test_unpack_file(mockgz: pathlib.Path) -> None:
     """Unpack the fixture gzip"""
 
     try:
+        download_itemlist.unpack_itemlist(mockgz)
+        assert mockgz.with_suffix("").exists()
 
-        download_itemlist.unpack_itemlist(MOCK_GZ)
-        assert MOCK_GZ.with_suffix("").exists()
+        body = open(mockgz.with_suffix(""), "r").read()
 
-        body = open(MOCK_GZ.with_suffix(""), "r").read()
-
-        assert "This is just a test file" in body
+        assert MOCK_GZ_BODY in body
 
     finally:
 
-        os.remove(MOCK_GZ.with_suffix(""))
+        os.remove(mockgz.with_suffix(""))

aaedaabd11e72303b3aebf5af6176556112bd238
Preocts <preocts@gmail.com>
2021-09-08 22:11:30 -0400

    Inventory client
    Create a worker that can load an inventory. Future iterations on this
    will include referencing item list manager to create links for each
    inventory item.

--- eqcharinfo/inventoryclient.py
+++ eqcharinfo/inventoryclient.py
@@ -2,6 +2,7 @@
 import csv
 import logging
 from pathlib import Path
+from typing import Generator
 from typing import List
 from typing import Optional
 
@@ -13,23 +14,35 @@ class InventoryClient:
 
     log = logging.getLogger(__name__)
 
-    def __init__(self, filepath: str, name: Optional[str] = None) -> None:
-        """Loads inventory file. If name is excluded, filename is used"""
-        self.filepath = Path(filepath).resolve()
-        self.name = self.filepath.name if name is None else name
-        self._raw_file = ""
-        self._inventory: List[Inventory] = []
+    def __init__(self, filepath: str, character_name: Optional[str] = None) -> None:
+        """Loads inventory file. If character name is excluded, filename is used"""
+        self._filepath = Path(filepath).resolve()
+        self._name = self._filepath.name if character_name is None else character_name
+        self._inventories: List[Inventory] = []
 
-        self.load_file()
+    def __iter__(self) -> Generator[Inventory, None, None]:
+        """Iterate over inventories"""
+        for inventory in self._inventories:
+            yield inventory
+
+    @property
+    def name(self) -> str:
+        """Character name for the inventory"""
+        return self._name
+
+    @property
+    def inventories(self) -> List[Inventory]:
+        """List of Inventory objects loaded"""
+        return self._inventories.copy()
 
     def load_file(self) -> None:
-        """Loads a file"""
-        self.log.info("Loading inventory file `%s`", self.filepath)
+        """Populates inventory from file in instance's filepath attribute"""
+        self.log.info("Loading inventory file `%s`", self._filepath)
 
-        with self.filepath.open(mode="r", encoding="utf-8") as infile:
+        with self._filepath.open(mode="r", encoding="utf-8") as infile:
             dictreader = csv.DictReader(infile, delimiter="\t")
             for row in dictreader:
-                self._inventory.append(
+                self._inventories.append(
                     Inventory(
                         location=row["Location"],
                         name=row["Name"],
---
+++ tests/inventoryclient_test.py
@@ -0,0 +1,41 @@
+from typing import Generator
+
+import pytest
+
+from eqcharinfo.inventoryclient import InventoryClient
+from eqcharinfo.models.inventory import Inventory
+
+MOCKFILE = "./tests/fixtures/inventory.txt"
+NAME = "inventory.txt"
+
+
+@pytest.fixture(scope="function", name="client")
+def fixture_client() -> Generator[InventoryClient, None, None]:
+
+    client = InventoryClient(MOCKFILE)
+
+    yield client
+
+
+def test_character_name(client: InventoryClient) -> None:
+    """Names should be explict or implied from filename"""
+    assert client.name == NAME
+
+    newclient = InventoryClient("nofile", "rooHappy")
+
+    assert newclient.name == "rooHappy"
+
+
+def test_load_file(client: InventoryClient) -> None:
+    """Load the file, assert some basic rules"""
+    client.load_file()
+
+    assert len(client.inventories)
+    assert isinstance(client.inventories[-1], Inventory)
+
+
+def test_iterator(client: InventoryClient) -> None:
+    """YAGNI feature of __iter__"""
+    client.load_file()
+    for inventory in client:
+        assert isinstance(inventory, Inventory)

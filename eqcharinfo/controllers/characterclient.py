d402077e9b51090394325a3ce375bc1e9d42fb99
Preocts <preocts@gmail.com>
2021-09-21 16:33:03 -0400

    Rename attribute for clarity of function

--- eqcharinfo/controllers/characterclient.py
+++ eqcharinfo/controllers/characterclient.py
@@ -17,18 +17,18 @@ class CharacterClient:
         """Controller for accessing character inventory data"""
         self.log = logging.getLogger(__name__)
 
-        self.character_client = CharacterInventoryProvider(config["CHARACTERS"])
+        self.character_provider = CharacterInventoryProvider(config["CHARACTERS"])
         self.max_results = config["DEFAULT"].getint("max_search_results", 10)
 
     def init_client(self) -> None:
         """Performs loading and setup"""
         self.log.debug("Loading characters from file...")
-        self.character_client.load_all_characters()
-        self.log.debug("Complete. Loaded %d characters", len(self.character_client))
+        self.character_provider.load_all_characters()
+        self.log.debug("Complete. Loaded %d characters", len(self.character_provider))
 
     def character_list(self) -> list[str]:
         """List of loaded characters, can be empty"""
-        return self.character_client.characters
+        return self.character_provider.characters
 
     def search_character(
         self,
@@ -36,7 +36,7 @@ class CharacterClient:
         search_string: str,
     ) -> list[GeneralSearchResult]:
         """Searches a specific character, returns best matching items"""
-        slots = self.character_client.get_character_slots(character_name)
+        slots = self.character_provider.get_character_slots(character_name)
         search_items = {slot.name: slot.id for slot in slots}
         search = fuzzy_search.search(search_string, search_items, self.max_results)
         return self._render_general_search_results(search)
@@ -44,8 +44,8 @@ class CharacterClient:
     def search_all(self, search_string: str) -> list[GeneralSearchResult]:
         """Searches all characters for best match of search_string"""
         search_items: dict[str, str] = {}
-        for character in self.character_client.characters:
-            slots = self.character_client.get_character_slots(character)
+        for character in self.character_provider.characters:
+            slots = self.character_provider.get_character_slots(character)
             search_items.update({slot.name: slot.id for slot in slots})
         search = fuzzy_search.search(search_string, search_items, self.max_results)
         return self._render_general_search_results(search)
@@ -54,14 +54,14 @@ class CharacterClient:
         self, character_name: str, item_id: str
     ) -> list[SpecificSearchResult]:
         """List specific item results from specific character"""
-        slots = self.character_client.get_character_slots(character_name)
+        slots = self.character_provider.get_character_slots(character_name)
         results = [slot for slot in slots if slot.id == item_id]
         return self._render_specific_search_results(results)
 
     def get_slots_all(self, item_id: str) -> dict[str, list[SpecificSearchResult]]:
         """By character name: list of item results found in character"""
         result: dict[str, list[SpecificSearchResult]] = {}
-        for character in self.character_client.characters:
+        for character in self.character_provider.characters:
             result[character] = self.get_slots_character(character, item_id)
         return result
 
